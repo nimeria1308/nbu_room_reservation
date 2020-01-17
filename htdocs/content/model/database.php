8f001e2c3c5ec6bca1a576733e5336b0e050dbd7
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2020-01-17 13:55:01 +0200

    Main page calendar load

---
+++ htdocs/content/model/database.php
@@ -0,0 +1,5 @@
+<?php
+$db = new mysqli("localhost", "root", "", "library");
+$db->set_charset("utf8");
+
+?>
--- htdocs/content/model/room.php
+++ htdocs/content/model/room.php
@@ -1,23 +1,46 @@
 <?php
 
 function get_rooms()
-{
-    # TODO: backend
-    return [
-        0 => [
-            "title" => "Семинарна зала",
-            "img_url" => "seminars-hall.jpg",
-            "color" => "dodgerblue",
-        ],
-        1 => [
-            "title" => "Зала за колективна работа",
-            "img_url" => "collective-work-hall.jpg",
-            "color" => "hotpink",
-        ],
-    ];
+{   //Load database
+    require_once "database.php";
+
+    //SQL querry
+    $search_id_result = $db->query("SELECT * FROM room");
+
+    //Check if the querry is successful
+    if(mysqli_num_rows($search_id_result)!=0){
+        //Load the rows into array one by one
+        while( ($row = $search_id_result->fetch_array())!=0){
+            $room[$row['room_id']]=[
+                "title" => $row['room_name'],
+                "img_url" => $row['image_path'],
+                "color" => $row['color'],
+            ];
+        }
+        return $room;
+    }
 }
 
-function get_room($id) {
-    # TODO: backend
-    return get_rooms()[$id];
+function get_room($id) { 
+    //Load database   
+    require_once "database.php";
+
+    //SQL querry
+    $search_id_result = $db->query("SELECT * FROM room WHERE room_id=$id");
+    //Check if the querry is successful
+    if(mysqli_num_rows($search_id_result)!=0){
+        //Load the row into array
+        $row = $search_id_result->fetch_array();
+        $room = [
+            $row['room_id'] => [
+                "title" => $row['room_name'],
+                "img_url" => $row['image_path'],
+                "color" => $row['image_path'],
+            ]
+        ];
+   
+        return $room[$id];
+    }
+
+    //return get_rooms()[$id];
 }
