26d794a32e39c7d2881659f76b639207cc0c6a8e
Iliyan <isyarov69@gmail.com>
2020-01-30 13:13:09 +0200

    tasks

--- htdocs/content/model/room.php
+++ htdocs/content/model/room.php
@@ -21,6 +21,28 @@ function get_rooms()
     }
 }
 
+//get all with order by field by choice
+function get_rooms_sorted($field)
+{   //Load database
+    require_once "database.php";
+
+    //SQL querry
+    $search_id_result = $db->query("SELECT * FROM room ORDER BY $field;");
+
+    //Check if the querry is successful
+    if(mysqli_num_rows($search_id_result)!=0){
+        //Load the rows into array one by one
+        while( ($row = $search_id_result->fetch_array())!=0){
+            $room[$row['room_id']]=[
+                "title" => $row['room_name'],
+                "img_url" => $row['image_path'],
+                "color" => $row['color'],
+            ];
+       }
+        return $room;
+    }
+}
+
 function get_room($id) { 
     //Load database   
     require_once "database.php";
