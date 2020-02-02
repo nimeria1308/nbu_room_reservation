516df7773f5222b299dd27fcda1f87165c3c8891
Simona Dimitrova <nimeria1308@gmail.com>
2020-02-02 23:31:38 +0200

    Added dates/times to new event

--- htdocs/content/pages/new_event.php
+++ htdocs/content/pages/new_event.php
@@ -8,11 +8,13 @@ $room = get_room($room_id);
 
 echo $room['title'];
 
+$now = new DateTime();
+
 if (isset($_GET['start']) and isset ($_GET['end'])) {
     $event_start = DateTime::createFromFormat(DateTime::ISO8601, $_GET['start']);
     $event_end = DateTime::createFromFormat(DateTime::ISO8601, $_GET['end']);
 } else {
-    $event_start = new DateTime();
+    $event_start = $now;
     $event_end = clone $event_start;
     $event_end->modify('+1 hour');
 }
@@ -20,6 +22,8 @@ if (isset($_GET['start']) and isset ($_GET['end'])) {
 # Room view
 $t = new MyView('event.phtml');
 $t->room = $room;
-$t->event_start = $event_start;
-$t->event_end = $event_end;
+$t->min_date = $now->format("Y-m-d");
+$t->date = $event_start->format("Y-m-d");
+$t->start_time = $event_start->format("H:i");
+$t->end_time = $event_end->format("H:i");
 $t->render();
