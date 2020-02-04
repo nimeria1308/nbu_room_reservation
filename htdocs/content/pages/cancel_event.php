8757e3600e506ec57e5783b08e882138355eb25a
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2020-02-04 14:49:44 +0200

    added call for delete_reservation function

--- htdocs/content/pages/cancel_event.php
+++ htdocs/content/pages/cancel_event.php
@@ -1,11 +1,15 @@
 <?php
-    if(isset($_GET['id']&&!empty($_GET['id']))){
-        $event_id=$_GET['id'];
-        //TODO:
-        //Adds function to cancel event
-    
-    
-        ////////////////
+		require_once('model/event.php');
+		require_once('model/database.php');
+
+    if(isset($_GET['id'])&&!empty($_GET['id'])){
+				$event_id=$_GET['id'];
+				
+				$find_room_id=$db->query("SELECT room_id FROM events WHERE event_id=$event_id");
+				if(mysqli_num_rows($find_room_id)!=0){
+					$row=$find_room_id->fetch_array();
+					delete_reservation($event_id,$row['room_id']);
+				}
     }
 
     header("Location: localhost:8080/rooms");  
