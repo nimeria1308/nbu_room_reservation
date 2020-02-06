8094386908e94ae88ef5e1215a4ca93d7e830ad5
Simona Dimitrova <nimeria1308@gmail.com>
2020-02-06 02:17:08 +0200

    Removed external packages. FullCalendar is used as a inplace script/css

--- external/build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-for /D %%f in (*) do (
-    start /b /w /d "%%f" cmd.exe /c "npm install" && start /b /w /d "%%f" cmd.exe /c "npm run build"
-)
--- start_server.bat
+++ start_server.bat
@@ -9,11 +9,6 @@ rem init database if not there
 %XAMPP_DIR%/php/php setup_database.php %*
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-rem compile external dependencies
-echo Compiling external dependencies
-start /b /w /d external cmd.exe /c build.bat
-if %errorlevel% neq 0 exit /b %errorlevel%
-
 rem create the logs dir if missing
 if not exist logs\ mkdir logs
 
